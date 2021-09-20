95e31d0d01432da9037e37b25e1299c4db86f06c
Alex Lunyov <isuntc@gmail.com>
2021-09-20 11:32:40 +0200

    SCALE_FACTOR derrived from device pixel density

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,18 @@
 export const isTooBright = (color) => color.reduce((a, b) => a + b, 0) > 340;
-export const SCALE_FACTOR = 2; // todo: calc from dpi
+
+export const SCALE_FACTOR = (() => {
+  const ctx = document.createElement("canvas").getContext("2d"),
+    dpr = window.devicePixelRatio || 1,
+    bsr =
+      ctx.webkitBackingStorePixelRatio ||
+      ctx.mozBackingStorePixelRatio ||
+      ctx.msBackingStorePixelRatio ||
+      ctx.oBackingStorePixelRatio ||
+      ctx.backingStorePixelRatio ||
+      1;
+
+  return dpr / bsr;
+})();
 
 const downloadLink = document.createElement("a");
 document.body.appendChild(downloadLink);
